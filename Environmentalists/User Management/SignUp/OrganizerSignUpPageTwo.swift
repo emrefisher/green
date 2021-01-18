a50276acf6c3ff526a0eacb67a17effafc8f44bc
Ian Campbell <campbell23567@gmail.com>
2021-01-17 22:23:26 -0500

    password page error handling

--- Environmentalists/User Management/SignUp/OrganizerSignUpPageTwo.swift
+++ Environmentalists/User Management/SignUp/OrganizerSignUpPageTwo.swift
@@ -69,7 +69,61 @@ struct OrganizerSignUpPageTwo: View {
                             .padding(.trailing, 17.5)
                     }
                 }
+                HStack {
+                    Button("Back", action: {
+                        self.organizerSignUpManager.pageNumber -= 1
+                    })
+                    
+                    Button("Next", action: {
+                        if validatePasswords() == nil {
+                            self.organizerSignUpManager.pageNumber += 1
+                        }
+                    }).alert(isPresented: self.$alert) {
+                        Alert(title: Text("Password Error"), message: Text(self.errorMessage), dismissButton: .default(Text("OK")))
+                    }
+                }
                 
             }.padding(.horizontal, UIScreen.main.bounds.width/20)
+    }
+    
+    private func isPasswordValid(password: String) -> Bool {
+        
+        let passwordTest = NSPredicate.init(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+        
+    }
+    
+    private func doPasswordsMatch(password: String, confirmedPassword: String) -> Bool {
+        
+        if password != confirmedPassword {
+            return false
+        }
+        return true
+    }
+    
+    private func validatePasswords() -> String? {
+        let cleanedPassword = self.organizerSignUpManager.password.trimmingCharacters(in: .whitespacesAndNewlines)
+        let cleanedConfirmedPassword = self.organizerSignUpManager.confirmedPassword.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if cleanedPassword == "" || cleanedConfirmedPassword == "" {
+            self.errorMessage = "Please fill in both fields."
+            self.alert.toggle()
+            return "Error"
+        }
+        
+        if isPasswordValid(password: cleanedPassword) == false {
+            self.errorMessage = "Please make sure your password is at least 8 characters and contains a capital letter and a number."
+            self.alert.toggle()
+            return "Error"
+        }
+        
+        if doPasswordsMatch(password: cleanedPassword, confirmedPassword: cleanedConfirmedPassword) == false {
+            self.errorMessage = "Please make sure your password entries match."
+            self.alert.toggle()
+            return "Error"
+        }
+        
+        return nil
+        
     }
 }
