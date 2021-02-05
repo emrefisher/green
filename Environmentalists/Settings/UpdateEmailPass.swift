dbb61f041caf1f7b75a416deddfd389a2b0c0696
Matthew <hanfmatthew@gmail.com>
2021-02-05 15:27:44 -0500

    (Added a update email and password file, it is not linked to the settings page. Update password should work but update email does not. When you press update email, it currently also sends you to updating password.)

--- Environmentalists/Settings/UpdateEmailPass.swift
+++ Environmentalists/Settings/UpdateEmailPass.swift
@@ -1,13 +1,13 @@
 //
-//  PasswordReset.swift
+//  Update Email and Password.swift
 //  Environmentalists
 //
-//  Created by Ian Campbell on 9/3/20.
+//  Created by Jess Hanf on 2/5/21.
 //
 
 import SwiftUI
 
-struct PasswordReset: View {
+struct UpdateEmailPass: View {
     
     @EnvironmentObject var sessionManager: UserSessionManager
     @State private var email = ""
@@ -26,14 +26,15 @@ struct PasswordReset: View {
                 }
                 Spacer()
             }.padding(.leading, 25.0)
-            Text("Reset Password")
+            Text("Update Email and Password")
                 .font(.headline)
                 .fontWeight(.bold)
                 .multilineTextAlignment(.center)
                 .padding(.vertical, 20.0)
-            Text("Provide your information below to change the password to your Environmend Account")
+            Text("Provide your information below to change the email and/or password to your Environmend Account")
+                .multilineTextAlignment(.center)
                 .lineLimit(nil)
-                .padding(.trailing, 30.0)
+                .padding(.horizontal, 30.0)
                 .padding(.bottom, 25.0)
             TextField("Email", text: self.$email).padding(.horizontal, 25)
                 .disableAutocorrection(true)
@@ -41,21 +42,34 @@ struct PasswordReset: View {
             Button(action: {
                 self.sessionManager.resetPassword(email: email)
             }) {
-                Text("Reset Password")
-            }
+                Text("Update Password")
+                    
+            }.padding()
+            
+            Button(action: {
+                self.sessionManager.resetPassword(email: email)
+                
+            }) {
+                
+                
+        
+                Text("Update Email")
+           
+            }.padding()
             
             Spacer()
-            
-            
         }
         .alert(isPresented: self.$sessionManager.alert) {
             Alert(title: Text("Password Reset Error"), message: Text(self.sessionManager.errorMessage), dismissButton: .destructive(Text("OK")))
+            
+            
+            
         }
     }
 }
 
-struct PasswordReset_Previews: PreviewProvider {
+struct UpdateEmailPass_Previews: PreviewProvider {
     static var previews: some View {
-       PasswordReset() .environmentObject(UserSessionManager())
+       UpdateEmailPass() .environmentObject(UserSessionManager())
     }
 }
