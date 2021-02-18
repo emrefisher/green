66f772ef0bfd5ca40e49da2a307c52e05c6b75c2
Ian Campbell <campbell23567@gmail.com>
2021-02-17 22:33:50 -0500

    event page

--- Environmentalists/Event Management/SearchDirectoryView/EventPage.swift
+++ Environmentalists/Event Management/SearchDirectoryView/EventPage.swift
@@ -26,9 +26,7 @@ struct EventPage: View {
     @EnvironmentObject var currentUser: CurrentUser
     @State private var actEvents = [Event]()
     @State private var isAttending = false
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    @State private var coordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
-    @State var markers: [Marker]?
+    @ObservedObject var mapManager = MapManager()
     //@ObservedObject var num = monitorAttendees()
     
     var body: some View {
@@ -103,8 +101,8 @@ struct EventPage: View {
                             
                             VStack {
                                 
-                                Map(coordinateRegion: $region,
-                                    annotationItems: markers ?? []) { marker in
+                                Map(coordinateRegion: $mapManager.region,
+                                    annotationItems: mapManager.markers ?? []) { marker in
                                            marker.location
                                 }
                                 }.frame(width: UIScreen.main.bounds.width*7/8, height: UIScreen.main.bounds.height/4)
@@ -138,17 +136,7 @@ struct EventPage: View {
                 self.isAttending = true
             }
             
-            let geocoder = CLGeocoder()
-                       geocoder.geocodeAddressString(self.event.location) {
-                           placemarks, error in
-                           let placemark = placemarks?.first
-                        let locationLatitude = placemark?.location?.coordinate.latitude ?? 0.0
-                        let locationLongitude = placemark?.location?.coordinate.longitude ?? 0.0
-                           self.region.center.latitude = locationLatitude
-                           self.region.center.longitude = locationLongitude
-                        self.coordinates = CLLocationCoordinate2D(latitude: locationLatitude, longitude: locationLongitude)
-                        self.markers = [Marker(location: MapMarker(coordinate: self.coordinates, tint: .red))]
-                   }
+            mapManager.getGeoLocation(event: event)
          
         }
         
@@ -224,3 +212,24 @@ struct closedRSVP: ButtonStyle {
     }
 }
 
+class MapManager: ObservableObject {
+    
+    @Published var coordinates = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @Published var markers: [Marker]?
+    
+    func getGeoLocation(event: Event) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(event.location) {
+            placemarks, error in
+            let placemark = placemarks?.first
+            let locationLatitude = placemark?.location?.coordinate.latitude ?? 0.0
+            let locationLongitude = placemark?.location?.coordinate.longitude ?? 0.0
+            self.region.center.latitude = locationLatitude
+            self.region.center.longitude = locationLongitude
+            self.coordinates = CLLocationCoordinate2D(latitude: locationLatitude, longitude: locationLongitude)
+            self.markers = [Marker(location: MapMarker(coordinate: self.coordinates, tint: .red))]
+        }
+    }
+    
+}
